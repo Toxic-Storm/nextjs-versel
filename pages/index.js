ffa4f81f18e903c9286710eb66c8b828045ffae5
João Pedro Viana Freitas <thewagaming@gmail.com>
2021-02-06 01:41:23 -0300

    Adição Inicial

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+//Sem Função
+function Home() {
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <Contador />
+        </div>
+    )
+}
+//Contador
+function Contador() {
+    const[contador, setContador] = useState(1);
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home
