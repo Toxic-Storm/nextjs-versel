18c2e55ec1a9d711510ecee7e99522c46cc27706
João Pedro Viana Freitas <thewagaming@gmail.com>
2021-02-06 02:24:50 -0300

    Update tempo.js

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
 function tempo(request, response) {
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
+    const primaryemailResponse = await fetch(`https://api.convertkit.com/v3/account?api_secret=${apiSecret}`);
+    const primaryemailResponseJson = await primaryemailResponse.json();
+    const email = primaryemailResponseJson.primary_email_address;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        email: email
     })
 }
 
